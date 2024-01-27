ea1e3fc413382fd842684fc75bafc30187996769
MuhammadAreeb30 <almostevil55@gmail.com>
2024-01-27 13:45:31 +0500

    Done

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    updateTodo: (state, action) => {
+      state.todos = state.todos.map((todo) =>
+        todo.id === action.payload.id ? action.payload.todo : todo
+      );
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, updateTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
